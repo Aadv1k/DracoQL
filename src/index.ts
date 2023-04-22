76d1dbff51df2be733bd385da855a10880644413
Aadv1k <aadv1k@outlook.com>
2023-04-22 19:26:56 +0530

    Add tests and remove dev code

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,6 @@ import interpreter from './Interpreter';
 import parser from "./Parser";
 import lexer from "./Lexer";
 
-eval(`
-  VAR data = FETCH "https://api.kanye.rest" 
-    AS JSON
-  VAR beta = EXTRACT "quote" FROM data AS JSON
-`, (ctx) => {
-  console.log(ctx.getVar("beta"));
-});
-
 export {
   interpreter,
   lexer,
